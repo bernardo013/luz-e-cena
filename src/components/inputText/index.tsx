f6f82ca133a15a6c368dfdd07ebac7988c5c12de
bernardo013 <105383046+bernardo013@users.noreply.github.com>
2026-01-09 09:41:47 -0300

    mudanças de caseSensitive

--- src/components/inputText/index.tsx
+++ src/components/inputText/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './InputText.module.css'
+import styles from './inputText.module.css'
 
 
 const InputText = ({...props}: React.InputHTMLAttributes<HTMLInputElement>) => {
